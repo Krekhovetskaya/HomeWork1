7cb426fb251726c43469e37081ced55d7e39ed78
Elizaveta Krekhovetskaya <okinaliza@gmail.com>
2023-02-11 00:46:17 +0300

    Решение задачи про три числа и максимальное

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write ("Введите число ");
+int numberA = int.Parse (Console.ReadLine()!);
+Console.Write ("Введите второе число ");
+int numberB = int.Parse (Console.ReadLine()!);
+Console.Write ("Введите третье число ");
+int numberE = int.Parse (Console.ReadLine()!);
+int max = numberA;
+
+if (numberA > max)
+{
+    max = numberA;
+}
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberE > max)
+{
+    max = numberE;
+}
+Console.Write ("max = ");
+Console.WriteLine (max);

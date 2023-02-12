2eb27f12279321cf992217825b2afefbeb93d55f
Elizaveta Krekhovetskaya <okinaliza@gmail.com>
2023-02-12 15:22:21 +0300

    задача на определение четного

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число ");
+int numberA = int.Parse (Console.ReadLine()!);
+
+if (numberA % 2 == 0)
+{
+    Console.Write ("Chetnoe");
+}
+else
+{
+    Console.Write ("Nechetnoe");
+}

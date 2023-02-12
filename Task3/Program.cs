45ec4964e1f23988cffda8365d4aee28a55ee86c
Elizaveta Krekhovetskaya <okinaliza@gmail.com>
2023-02-13 00:15:16 +0300

    задача на вывод четных до заданного числа

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число ");
+int numberA = int.Parse (Console.ReadLine()!);
+int i = 1;
+while (i <= numberA)
+{
+   if (i % 2 == 0)
+   {
+    Console.Write(i + " ");
+   }
+   i++;
+}
